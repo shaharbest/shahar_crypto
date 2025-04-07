42eb0a84c5123e2a3ac9a8db94883d4133cff44a
Shahar Best <bestshahar9@gmail.com>
2025-04-07 19:28:32 +0300

    Typescript

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,17 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
-import { meteor } from 'meteor-vite/plugin';
+import { meteor } from 'meteor-vite/plugin'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [
-        meteor({
-          clientEntry: 'client/entry-vite.jsx',
-        }),
-        react({
-            jsxRuntime: 'classic',
-        }),
-    ],
-    optimizeDeps: {
-        exclude: ['@meteor-vite/react-meteor-data'], 
-    }
+  plugins: [
+    meteor({
+      clientEntry: 'client/entry-vite.tsx', // Ensure this entry is TypeScript (tsx)
+    }),
+    react({
+      jsxRuntime: 'automatic', // Use the new JSX runtime (recommended for TypeScript)
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ['@meteor-vite/react-meteor-data'],
+  },
 })
